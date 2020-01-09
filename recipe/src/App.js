b55785187c8ee7e690dcc964302eacc0d82bd084
costindumitrescu <dumitrescucostin20@gmail.com>
2020-01-09 16:49:33 -0500

    Preparing to style

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -8,21 +8,33 @@ const APP_ID = "0c09d9f5";
 const APP_KEY = " 9d04669214936e325e90a2e41aff3d84";
 const [recipies, setRecipies] = useState([]);
 const [search, setSearch] = useState('');
+const [query, setQuery] = useState('chicken');
 
 
 useEffect(() =>{
 getRecipies();
-}, []);
+}, [query]);
 
 const getRecipies = async () => {
-  const response =  await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+  const response =  await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
   const data = await response.json();
   setRecipies(data.hits);
-}
+};
+
+const updateSearch = e => {
+setSearch(e.taget.value);
+};
+
+const getSearch = e => {
+e.preventDefault();
+setQuery(search);
+setSearch('');
+};
+
   return (
     <div className = "App">
-    <form className= "search-form">
-      <input className= "search-bar" type="" value={search} onChange={}/>
+    <form onSubmit={getSearch} className= "search-form">
+      <input className= "search-bar" type="" value={search} onChange={updateSearch}/>
       <button onClick={() => setCounter(counter + 1)} className= "search-button" type="submit">
         {counter}
       </button>
--- recipe/src/Recipe.js
+++ recipe/src/Recipe.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 
-const Recipe = ({title, calories, image}) => {
+const Recipe = ({title, calories, image, ingredients, ingredientsl, total, health}) => {
   return(
     <div>
     <h1>{title}</h1>
     <p>{calories}</p>
     <img src={image} alt="" />
-    <p>{ingredients}</p>
+    <ol> {ingredients.map(ingredient => (
+      <li>{ingredient.text}</li>
+    ))}</ol>
     <p>{ingredientsl}</p>
     <p>{total}</p>
     <p>{health}</p>
